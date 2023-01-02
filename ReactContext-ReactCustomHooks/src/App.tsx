4781628d272384098bad9db5943a2961061f6c93
Khalid Najmeddine <knajm574@gmail.com>
2023-01-01 19:45:13 -0700

    feat: solution => React-Context + ReactCustomHooks

--- ReactContext-ReactCustomHooks/src/App.tsx
+++ ReactContext-ReactCustomHooks/src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, {useState, useEffect, createContext, useContext} from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Pokemon {
+  id: number
+  name: string
+  type: string[]
+  hp: number
+  attack: number
+  defense: number
+  special_defense: number
+  special_attack: number
+  speed: number
+}
+
+function usePokemonSource(): {pokemon: Pokemon[]} {
+  const[pokemon, setPokemon] = React.useState<Pokemon[]>([])
+
+  React.useEffect(() => {
+    fetch("/pokemon.json").then((response) => response.json()).then((data) => setPokemon(data))
+  }, [])
+
+  return {pokemon}
+}
+
+const PokemonContext = React.createContext<ReturnType<typeof usePokemonSource> | undefined>(undefined)
+
+function usePokemon() {
+  return React.useContext(PokemonContext)! // ! tells typescript that usePokemon() will always return a value of PokemonContext that will never be undefined
+}
+
+function PokemonList() {
+  const {pokemon} = usePokemon()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      {pokemon.map((mapPokemon) => (
+        <div key={mapPokemon.id}>{mapPokemon.name}</div>
+      ))}
     </div>
   )
 }
 
-export default App
+export default function App() {
+
+  return (
+    <PokemonContext.Provider value={usePokemonSource()}>
+      <PokemonList />
+    </PokemonContext.Provider>
+  )
+}
