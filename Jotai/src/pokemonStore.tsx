013d145c07ec297a806f7a2e327866e86da90c9f
Khalid Najmeddine <knajm574@gmail.com>
2023-01-03 11:36:05 -0700

    feat: solution using Jotai

---
+++ Jotai/src/pokemonStore.tsx
@@ -0,0 +1,31 @@
+import {atom} from "jotai"
+import {atomsWithQuery} from "jotai-tanstack-query"
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+export const searchAtom = atom("")
+
+const [allPokemon] = atomsWithQuery<Pokemon[]>(() => ({
+  queryKey: ["pokemon"],
+  queryFn: () => fetch("/pokemon.json").then((response) => response.json())
+}))
+
+export const pokemonAtom = atom((get) => {
+  const search = get(searchAtom).toLowerCase()
+  const allSearchedPokemon = get(allPokemon)
+  return allSearchedPokemon.filter((filterPokemon) => filterPokemon.name.toLowerCase().includes(search))
+})
+
+export const sortedPokemonAtom = atom((get) => {
+  const pokemon = get(pokemonAtom)
+  return pokemon.slice(0,10).sort((pokemonA, pokemonB) => pokemonA.name.localeCompare(pokemonB.name))
+})
